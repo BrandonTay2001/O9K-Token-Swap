3a2c3ba6a11a139978f32cb4a52f31c935442e3a
BrandonTay2001 <brandontay2001@gmail.com>
2021-08-24 22:26:18 +0800

    preparations for testnet deployment

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,24 @@
 require('babel-register');
 require('babel-polyfill');
 
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = require('./secrets.json').mnemonic;
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://speedy-nodes-nyc.moralis.io/6fae173edc4997c10972840e/eth/ropsten'),
+      network_id: 3,
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
     },
   },
   contracts_directory: './src/contracts/',
